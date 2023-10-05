9edc52f368183d4595c50db717d0fa0ec713c9e5
Lucas Ivisson <lucasivisson1@gmail.com>
2023-10-05 19:40:51 -0300

    ✨ introduce status bar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import {
   useFonts,
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
