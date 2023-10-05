dd7ef5ec16b6877a40c11d4c97e40a952d2750e8
Lucas Ivisson <lucasivisson1@gmail.com>
2023-10-05 19:33:40 -0300

    ✨ introduce loading components

--- App.tsx
+++ App.tsx
@@ -6,14 +6,15 @@ import {
 } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
-import { ActivityIndicator } from "react-native";
+
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
