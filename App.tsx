52ad682df24b6ff961385a4b852c5e742648fa95
Lucas Ivisson <lucasivisson1@gmail.com>
2023-10-05 19:21:30 -0300

    ✨ introduce costum fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { Groups } from "@screens/Groups";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
