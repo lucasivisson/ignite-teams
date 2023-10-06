a474ad7da8dc56cfa268f1b3e5c489a59d1fa27d
Lucas Ivisson <lucasivisson1@gmail.com>
2023-10-06 17:33:40 -0300

    🚧 working with flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
@@ -5,11 +8,17 @@ import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState(["Galera do Bananau", "Bananau"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
