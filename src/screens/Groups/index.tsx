dde1bdb60f1333271b119223986bb61d35190363
Lucas Ivisson <lucasivisson1@gmail.com>
2023-10-06 17:59:23 -0300

    ✨ introduce empty list component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState(["Galera do Bananau", "Bananau"]);
+  const [groups, setGroups] = useState([]);
 
   return (
     <Container>
@@ -18,6 +19,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
